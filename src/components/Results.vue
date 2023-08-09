c5af60c17e3c1bcb12ab3f06635dec665bedf4f1
Eric boadu <ericboadu888@gmail.com>
2023-08-09 14:28:55 -0400

    reaction timer (vue.js)

---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <p>Reaction time: {{ score }} ms</p>
+      <p class="rank">{{ rank }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['score'], 
+    data() {
+        return{
+            rank: null
+        }
+    }, 
+    mounted(){
+        if (this.score < 120){
+            this.rank = 'Are you Goku!?? '    
+        }else if (this.score < 250 ){
+            this.rank = 'Like lightning ' 
+        } else if (this.score < 400){
+            this.rank = 'good Reflexes'
+        } else {
+            this.rank = 'Maaaaan Your slow.......'
+        }
+    }
+  }
+  </script>
+  
+  <style>
+  .rank{
+    font-size: 1.4em; 
+    color: #6e4eaa; 
+    font-weight: bold;
+  }
+  </style>
+  
